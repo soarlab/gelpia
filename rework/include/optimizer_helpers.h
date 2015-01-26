63c8358a7feacc468c9bd0cb9a5a62e751a500de
Ian Briggs <ianbriggs@comcast.net>
2015-01-26 13:40:36 -0700

    staging for swig interface to python

---
+++ rework/include/optimizer_helpers.h
@@ -0,0 +1,34 @@
+
+#ifndef OPTIMIZER_HELPERS_H
+#define OPTIMIZER_HELPERS_H
+
+#include "optimizer_types.h"
+
+/*
+ * Divides given interval box along longest dimension
+ * Arguments:
+ *          X - given box
+ * Return: vector of two new boxes
+ */
+std::vector<box_t> split_box(const box_t & X);
+
+
+/*
+ * Finds midpoint of given box
+ * Arguments:
+ *          X - given box
+ * Return: box whose dimentions align to the single midpoint
+ */
+box_t midpoint(const box_t & X);
+
+
+/*
+ * Caluclates the width of the box, the length of the longest dimension
+ * Arguments:
+ *          X - given box
+ * Return: width scalar
+ */
+double width(const box_t & X);
+
+
+#endif
