a5c140181bc33860bb85066cfaacdb43c08cc5ec
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2015-06-01 14:50:45 -0600

    priority solver fix

--- src/priority_serial_solver.py
+++ src/priority_serial_solver.py
@@ -6,27 +6,27 @@ def solve(X_0, x_tol, f_tol, func, procs_ignore, profiler):
     local_queue = Q.PriorityQueue()
     local_queue.put((0, 0, X_0))
     priority_fix = 0
-    f_best = GU.large_float("-inf");
+    f_best_low = GU.large_float("-inf")
+    f_best_high = GU.large_float("-inf")
     
     while not local_queue.empty():
         ignore_0, ignore_1, X = local_queue.get()
-
         fx = func(X)
         w = X.width()
         fw = fx.width()
-        
-        if (fx.upper() < f_best or
+        if (fx.upper() < f_best_low or
             w < x_tol or
             fw < f_tol):
-            continue
+            if f_best_high < fx.upper():
+                f_best_high = fx.upper()
         else:
             box_list = X.split()
             
             for b in box_list:
                 e = func(b.midpoint())
-                if(e.upper() > f_best):
-                    f_best = e.upper()
+                if(e.upper() > f_best_low):
+                    f_best_low = e.upper()
                 priority_fix += 1
                 local_queue.put((-e.upper(), priority_fix, b))
 
-    return f_best
+    return f_best_high
