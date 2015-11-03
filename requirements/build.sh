673e12e3c5e5d9115726e9813aab3caed99879b7
ian Briggs <ianbriggs@comcast.net>
2015-11-02 18:00:48 -0700

    auto build script made and tested, final path modifications need to be determined

---
+++ requirements/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SOURCE_LOCATION=$SCRIPT_LOCATION/Sources
+mkdir -p $SOURCE_LOCATION
+
+cd $SOURCE_LOCATION
+wget https://static.rust-lang.org/dist/rust-nightly-x86_64-unknown-linux-gnu.tar.gz -O rust_nightly.tar.gz
+mkdir -p rust_nightly && tar -xf rust_nightly.tar.gz -C rust_nightly --strip-components 1
+cd rust_nightly
+./install.sh --prefix=$SCRIPT_LOCATION
+
+cd $SOURCE_LOCATION
+wget http://lipforge.ens-lyon.fr/frs/download.php/162/crlibm-1.0beta4.tar.gz -O crlibm.tar.gz
+mkdir -p crlibm && tar -xf crlibm.tar.gz -C crlibm --strip-components 1
+cd crlibm
+export CFLAGS=-fPIC $CFLAGS
+export LDFLAGS=-fPIC $LDFLAGS
+./configure --enable-sse2 --prefix=$SCRIPT_LOCATION
+make
+make install
+export LIBRARY_PATH=$SCRIPT_LOCATION/lib:$LIBRARY_PATH
+export C_INCLUDE_PATH=$SCRIPT_LOCATION/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$SCRIPT_LOCATION/include:$CPLUS_INCLUDE_PATH
+
+cd $SOURCE_LOCATION
+wget http://downloads.sourceforge.net/project/gaol/gaol/4.2.0/gaol-4.2.0.tar.gz -O gaol.tar.gz
+mkdir -p gaol && tar -xf gaol.tar.gz -C gaol --strip-components 1
+cd gaol
+./configure  --with-mathlib=crlibm --enable-simd --disable-debug --disable-preserve-rounding --enable-optimize --disable-verbose-mode --prefix=$SCRIPT_LOCATION
+make
+make install
+
+
