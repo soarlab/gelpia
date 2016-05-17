1081bf280056f0c0c9812c057614876434dac7f6
Ian Briggs <ianbriggs@comcast.net>
2016-05-17 13:08:28 -0600

    greatly simplified parser

--- src/frontend/function_transforms/lexed_to_parsed.py
+++ src/frontend/function_transforms/lexed_to_parsed.py
@@ -1,210 +1,196 @@
 #!/usr/bin/env python3
 
 from function_to_lexed import *
+from function_to_lexed import _function_lexer
+from pass_manager import *
 
 import ply.yacc as yacc
 import sys
 
-# helpers
-def parse_compare(l, r):
-    if l[0] != r[0] or len(l) != len(r):
-        return False
-    if len(l) > 1:
-        return parse_compare(l[1], r[1])
-    return True
-
-def is_integer(t):
-    if t[0] == 'Neg':
-        return is_integer(t[1])
-    if t[0] == 'Integer':
-        return True
-    return False
 
 precedence = (
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIVIDE'),
-    ('right', 'BINPOW')
-    )
+  ("left", "PLUS", "MINUS"),
+  ("left", "TIMES", "DIVIDE"),
+  ("right", "INFIX_POW"),
+)
 
 
-def p_function_assign(t):
-    '''function : variable EQUALS expression SEMICOLON function'''
-    t[0] = [['Assign', t[1], t[3]], t[5]]
-    
-
-        
-def p_function_end(t):
-    '''function : expression
-                | expression SEMICOLON'''
-    t[0] = ['Return', t[1]]
-
-
-
-
-def p_expression(t):
-    '''expression : expression PLUS term
-                  | expression MINUS term'''
-    t[0] = [t[2], t[1], t[3]]
-
-def p_expression_passthrough(t):
-    '''expression : term'''
-    t[0] = t[1]
-
-
-
-
-def p_term(t):
-    '''term : term TIMES binary_pow
-            | term DIVIDE binary_pow'''
-    t[0] = [t[2], t[1], t[3]]
-
-def p_term_passthrough(t):
-    '''term : binary_pow'''
-    t[0] = t[1]
-
-
-
-
-def p_binary_pow(t):
-    '''binary_pow : binary_pow BINPOW binary_pow'''
-    if is_integer(t[3]):
-        t[0] = ['ipow', t[1], t[3]]
-    else:
-        t[0] = ['pow', t[1], t[3]]
-
-def p_binary_pow_passthrouh(t):
-    '''binary_pow : uniop'''
-    t[0] = t[1]
-
-
-
-def p_uniop_neg(t):
-    '''uniop : MINUS base'''
-    t[0] = ['Neg', t[2]]
-
-def p_uniop_passthrough(t):
-    '''uniop : base'''
-    t[0] = t[1]
-
-
-
-
-def p_base_passthrough(t):
-    '''base : variable
-            | const
-            | group
-            | func'''
-    t[0] = t[1]
-
-
-
-
-def p_name(t):
-    '''variable : NAME'''
-    t[0] = ['Variable', t[1]]
-
-
-
-
-def p_const_passthrough(t):
-    '''const : number
-             | interval'''
-    t[0] = t[1]
-
-
-
-
-def p_number_neg(t):
-    '''number : MINUS number'''
-    t[0] = ['Neg', t[2]]
-
-def p_number_pos(t):
-    '''number : PLUS number'''
-    t[0] = t[2]
-
-def p_number_integer(t):
-    '''number : INTEGER'''
-    t[0] = ['Integer', t[1]]
-
-def p_number_float(t):
-    '''number : FLOAT'''
-    t[0] = ['Float', t[1]]
-
-
-
-
-def p_interval_single(t):
-    '''interval : INTERVAL LPAREN number RPAREN'''
-    t[0] = ['Interval', t[3], t[3]]
-        
-def p_interval(t):
-    '''interval : INTERVAL LPAREN number COMMA number RPAREN'''
-    if parse_compare(t[3], t[5]):
-        t[0] = ['Interval', t[3], t[5]]
-    else:
-        t[0] = ['InputInterval', t[3], t[5]]
-
-def p_interval_brace_single(t):
-    '''interval : LBRACE number RBRACE'''
-    t[0] = ['Interval', t[2], t[2]]
-    
-def p_interval_brace(t):
-    '''interval : LBRACE number COMMA number RBRACE'''
-    if parse_compare(t[2], t[4]):
-        t[0] = ['Interval', t[2], t[4]]
-    else:
-        t[0] = ['InputInterval', t[2], t[4]]
-
-
-
-        
-def p_group_passthrough(t):
-    '''group : LPAREN expression RPAREN'''
-    t[0] = t[2]
-
-
-
-
-def p_func_uniop(t):
-    '''func : UNIOP LPAREN expression RPAREN'''
-    t[0] = [t[1], t[3]]
-
-def p_func_prefix_binop(t):
-    '''func : BINOP LPAREN expression COMMA expression RPAREN'''
-    if t[1] == 'pow' and is_integer(t[5]):
-        t[0] = ['ipow', t[3], t[5]]
-    else:
-        t[0] = [t[1], t[3], t[5]]
-
-
-
-        
-    
-def p_error(t):
-    print("Syntax error at '{}'".format(t))
+def p_function(t):
+  ''' function : variable EQUALS expression SEMICOLON function
+               | expression
+               | expression SEMICOLON '''
+  if len(t) == 6:
+    t[0] = [["Assign", t[1], t[3]], t[5]]
+  elif len(t) == 2 or len(t) == 3:
+    t[0] = ["Return", t[1]]
+  else:
+    print("Internal parse error in p_function")
     sys.exit(-1)
 
 
+def p_expression(t):
+  ''' expression : expression PLUS expression
+                 | expression MINUS expression
+                 | expression TIMES expression
+                 | expression DIVIDE expression
+                 | expression INFIX_POW expression
+                 | negation '''
+  if len(t) == 4:
+    if t[2] == '^':
+      t[0] = ["pow", t[1], t[3]]
+    else:
+      t[0] = [t[2], t[1], t[3]]
+  elif len(t) == 2:
+    t[0] = t[1]
+  else:
+    print("Internal parse error in p_expression")
+    sys.exit(-1)
 
-# Create parser on call and import
-function_parser = yacc.yacc(debug=0, write_tables=0)
 
-def runmain_parser(parser):
-    ''' Wrapper to allow parser to run with direct command line input '''
-    try:
-        filename = sys.argv[1]
-        with open(filename, 'r') as f:
-            data = f.read()
-    except IndexError:
-        sys.stdout.write('Reading from standard input (type EOF to end):\n')
-        data = sys.stdin.read()
+def p_negation(t):
+  ''' negation : MINUS negation
+               | base '''
+  if len(t) == 3:
+    typ = t[2][0]
+    if typ in ["Integer", "Float"]:
+      val = t[2][1]
+      if val[0] == '-':
+        t[0] = [typ, val[1:]]
+      else:
+        t[0] = [typ, '-'+val]
+    elif typ == "Neg":
+      t[0] = t[2][1]
+    else:
+      t[0] = ["Neg", t[2]]
+  elif len(t) == 2:
+    t[0] = t[1]
+  else:
+    print("Internal parse error in p_negation")
+    sys.exit(-1)
 
-    exp = parser.parse(data)
-    while type(exp[0]) is list:
-        print(exp[0])
-        exp = exp[1]
-    print(exp)
-    
-# On call run as a util, taking in text and printing the parsed version
+
+def p_base(t):
+  ''' base : variable
+           | interval
+           | const
+           | group
+           | func '''
+  t[0] = t[1]
+
+
+def p_variable(t):
+  ''' variable : NAME '''
+  t[0] = ["Variable", t[1]]
+
+
+def p_interval(t):
+  ''' interval : INTERVAL LPAREN   negconst COMMA    negconst RPAREN
+               | LBRACE   negconst COMMA    negconst RBRACE
+               | INTERVAL LPAREN   negconst RPAREN
+               | LBRACE   negconst RBRACE '''
+  if len(t) == 7:
+    left = t[3]
+    right = t[5]
+  elif len(t) == 6:
+    left = t[2]
+    right = t[4]
+  elif len(t) == 5:
+    left = t[3]
+    right = left
+  elif len(t) == 4:
+    left = t[2]
+    right = left
+  else:
+    print("Internal parse error in p_interval")
+    sys.exit(-1)
+
+  if left == right:
+    t[0] = ["ConstantInterval", left]
+  else:
+    t[0] = ["InputInterval", left, right]
+
+
+def p_negconst(t):
+  ''' negconst : MINUS negconst
+               | const '''
+  if len(t) == 3:
+    typ = t[2][0]
+    val = t[2][1]
+    if val[0] == '-':
+      t[0] = [typ, val[1:]]
+    else:
+      t[0] = [typ, '-'+val]
+  elif len(t) == 2:
+    t[0] = t[1]
+  else:
+    print("Internal parse error in p_negconst")
+    sys.exit(-1)
+
+
+def p_const(t):
+  ''' const : PLUS const
+            | integer
+            | float '''
+  if len(t) == 3:
+    t[0] = t[2]
+  elif len(t) == 2:
+    t[0] = t[1]
+  else:
+    print("Internal parse error in p_const")
+    sys.exit(-1)
+
+
+def p_integer(t):
+  ''' integer : INTEGER '''
+  t[0] = ["Integer", t[1]]
+
+
+def p_float(t):
+  ''' float : FLOAT '''
+  t[0] = ["Float", t[1]]
+
+
+def p_group(t):
+  ''' group : LPAREN expression RPAREN '''
+  t[0] = t[2]
+
+
+def p_func(t):
+  ''' func : BINOP LPAREN expression COMMA expression RPAREN
+           | UNIOP LPAREN expression RPAREN '''
+  if len(t) == 7:
+    t[0] = [t[1], t[3], t[5]]
+  elif len(t) == 5:
+    t[0] = [t[1], t[3]]
+  else:
+    print("Internal parse error in p_func")
+    sys.exit(-1)
+
+
+def p_error(t):
+  print("Syntax error at '{}'".format(t))
+  sys.exit(-1)
+
+
+
+
+
+
+
+
+_function_parser = yacc.yacc(debug=0, write_tables=1, optimize=1)
+
+def parse_function(text):
+  return _function_parser.parse(text, lexer=_function_lexer)
+
+def runmain():
+  data = get_runmain_input()
+  exp = parse_function(data)
+
+  print_exp(exp)
+
 if __name__ == "__main__":
-    runmain_parser(function_parser)
+  try:
+    runmain()
+  except KeyboardInterrupt:
+    print("\nGoodbye")
