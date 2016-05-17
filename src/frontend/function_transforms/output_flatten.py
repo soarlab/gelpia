4889e509d947dba64e4e1a5aaa08582f94a4226c
Ian Briggs <ianbriggs@comcast.net>
2016-05-17 13:11:53 -0600

    revamped expression flattener

---
+++ src/frontend/function_transforms/output_flatten.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+from pass_manager import *
+
+import sys
+
+
+def flatten(root, exp, inputs, consts, assign):
+  lp = ['(']
+  rp = [')']
+  cm = [',']
+  lb = ['[']
+  rb = [']']
+  
+  def _flatten(exp):
+    if type(exp[0]) is list:
+      return _flatten(exp[1])
+
+    if exp[0] in BINOPS:
+      return [exp[0]] + lp + _flatten(exp[1]) + cm + _flatten(exp[2]) + rp
+
+    if exp[0] in UNIOPS:
+      return [exp[0]] + lp + _flatten(exp[1]) + rp
+
+    if exp[0] in {"ConstantInterval", "Integer", "Float"}:
+      return lb + [exp[1]] + rb
+
+    if exp[0] in {"InputInterval"}:
+      inside = _flatten(exp[1]) + cm + _flatten(exp[2])
+      inside = [part for part in inside if part != lb and part != rb]
+      return lb + inside + rb
+
+    if exp[0] in {"Const"}:
+      return _flatten(consts[int(exp[1])])
+    
+    if exp[0] in {"Input"}:
+      return _flatten(inputs[exp[1]])
+
+    if exp[0] in {"Variable"}:
+      return _flatten(assign[exp[1]])
+
+    if exp[0] in {"Return"}:
+      return _flatten(exp[1])
+
+    print("lift_assigns error unknown: '{}'".format(exp))
+    sys.exit(-1)
+
+    
+  return ''.join(_flatten(exp))
+
+
+
+                                
+                                
+
+def runmain():
+  from lexed_to_parsed import parse_function
+  from pass_lift_inputs import lift_inputs
+  from pass_lift_consts import lift_consts
+  from pass_lift_assign import lift_assign
+  
+  data = get_runmain_input()
+  exp = parse_function(data)
+  inputs = lift_inputs(exp)
+  consts = lift_consts(exp, inputs)
+  assign = lift_assign(exp, inputs, consts)
+  flattened = flatten(exp, exp, inputs, consts, assign)
+  
+  print("flattened:")
+  print(flattened)
+  print()
+  print_exp(exp)
+  print()
+  print_inputs(inputs)
+  print()
+  print_consts(consts)
+  
+if __name__ == "__main__":
+  try:
+    runmain()
+  except KeyboardInterrupt:
+    print("\nGoodbye")
