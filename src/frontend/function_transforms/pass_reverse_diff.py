39125f21d830ef0f3023d7fd2c3c8d70437c38c1
ian Briggs <ianbriggs@comcast.net>
2016-08-15 10:22:44 -0600

    Allow reverse diff to use new internals

--- src/frontend/function_transforms/pass_reverse_diff.py
+++ src/frontend/function_transforms/pass_reverse_diff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from pass_manager import *
+from pass_utils import *
+from pass_lift_consts import lift_consts
 
 import collections
 import re
@@ -9,20 +10,18 @@ import subprocess
 import os.path as path
 import math
 
-def reverse_diff(exp, inputs, assigns, consts):
-  gradient = collections.OrderedDict([(k,None) for k in inputs])
+def reverse_diff(exp, inputs, assigns, consts=None):
+  gradient = collections.OrderedDict([(k,["Integer","0"][:]) for k in inputs])
 
   def _reverse_diff(exp, adjoint):
     tag = exp[0]
+
     if tag in {"Input"}:
-      if gradient[exp[1]] == None:
-        gradient[exp[1]] = adjoint
-        return
       old = gradient[exp[1]]
       gradient[exp[1]] =["+", old, adjoint]
       return
 
-    if tag in {"Const"}:
+    if tag in {"Const", "ConstantInterval", "PointInterval", "Integer", "Float"}:
       return
 
     if tag in {'+'}:
@@ -32,7 +31,7 @@ def reverse_diff(exp, inputs, assigns, consts):
 
     if tag in {'-'}:
       _reverse_diff(exp[1], adjoint)
-      _reverse_diff(exp[2], ["Neg", adjoint])
+      _reverse_diff(exp[2], ["neg", adjoint])
       return
 
     if tag in {'*'}:
@@ -46,26 +45,24 @@ def reverse_diff(exp, inputs, assigns, consts):
       upper = exp[1]
       lower = exp[2]
       _reverse_diff(exp[1], ["/", adjoint, lower])
-      _reverse_diff(exp[2], ["/", ["*", ["Neg", adjoint], upper], ["pow", lower, ["Integer", "2"]]]) #"-({})*{}/{}**2".format(adjoint, high, low))
+      _reverse_diff(exp[2], ["/", ["*", ["neg", adjoint], upper], ["pow", lower, ["Integer", "2"]]])
       return
 
     if tag in {"exp"}:
       expo = exp[1]
-      _reverse_diff(exp[1], ["*", ["exp", expo], adjoint]) #"exp({})*{}".format(base, adjoint))
+      _reverse_diff(exp[1], ["*", ["exp", expo], adjoint])
       return
 
     if tag in {"log"}:
       base = exp[1]
-      _reverse_diff(exp[1], ["/", adjoint, base]) #"1/{}*{}".format(base, adjoint))
+      _reverse_diff(exp[1], ["/", adjoint, base])
       return
 
     if tag in {"pow", "powi"}:
       base = exp[1]
       expo = exp[2]
       _reverse_diff(exp[1], ['*', adjoint, ['*', expo, ['powi', base, ['-', expo, ['Integer', "1"]]]]])
-                    #"{}*{}*{}**({}-1)".format(adjoint, expo, base, expo))
       _reverse_diff(exp[2], ['*', adjoint, ['*', ['log', base], ['powi', base, expo]]])
-                    #"{}*math.log({})*{}**{}".format(adjoint, base, base, expo))
       return
 
     if tag == "sqrt":
@@ -73,7 +70,7 @@ def reverse_diff(exp, inputs, assigns, consts):
       return
 
     if tag == "cos":
-      _reverse_diff(exp[1], ['*', ["Neg", ["sin", exp[1]]], adjoint])
+      _reverse_diff(exp[1], ['*', ["neg", ["sin", exp[1]]], adjoint])
       return
 
     if tag == "sin":
@@ -81,9 +78,7 @@ def reverse_diff(exp, inputs, assigns, consts):
       return
 
     if tag == "tan":
-      _reverse_diff(exp[1], ['*', ['+', ["Integer", "1"],
-                                   ["pow", ["tan", exp[1]], ["Integer", "2"]]],
-                             adjoint])
+      _reverse_diff(exp[1], ['*', ['+', ["Integer", "1"], ["pow", ["tan", exp[1]], ["Integer", "2"]]], adjoint])
       return
 
     if tag == "cosh":
@@ -95,24 +90,22 @@ def reverse_diff(exp, inputs, assigns, consts):
       return
 
     if tag == "tanh":
-      _reverse_diff(exp[1], ['*', ['-', ["Integer", "1"],
-                                   ["pow", ["tanh", exp[1]], ["Integer", "2"]]],
-                             adjoint])
+      _reverse_diff(exp[1], ['*', ['-', ["Integer", "1"], ["pow", ["tanh", exp[1]], ["Integer", "2"]]], adjoint])
       return
 
     if tag == "abs":
       _reverse_diff(exp[1], ['*', ["dabs", exp[1]], adjoint])
       return
 
-    if tag in {"Neg"}:
-      _reverse_diff(exp[1], ["Neg", adjoint]) #"-({})".format(adjoint))
+    if tag =="neg":
+      _reverse_diff(exp[1], ["neg", adjoint])
       return
 
-    if tag in {"Variable"}:
+    if tag == "Variable":
       _reverse_diff(assigns[exp[1]], adjoint)
       return
 
-    if tag in {"Return"}:
+    if tag == "Return":
       _reverse_diff(exp[1], adjoint)
       return
 
@@ -122,13 +115,15 @@ def reverse_diff(exp, inputs, assigns, consts):
     print("reverse_diff error unknown: '{}'".format(exp))
     sys.exit(-1)
 
-  #exp = single_assignment(exp, inputs, assigns, consts)
+
   _reverse_diff(exp, ["Integer", "1"])
   result = ["Box"]+[d for d in gradient.values()]
-  # for k in gradient:
-  #   new_exp = single_assignment(["Return", gradient[k]], inputs, assigns, consts)
-  #   gradient[k] = new_exp
-  return ["Tuple", exp, result]
+  retval = ["Return", ["Tuple", exp[1], result]]
+
+  if consts != None:
+    lift_consts(retval, inputs, assigns, consts)
+
+  return retval
 
 
 
@@ -139,34 +134,52 @@ def reverse_diff(exp, inputs, assigns, consts):
 
 def runmain():
   from lexed_to_parsed import parse_function
-  from pass_lift_inputs import lift_inputs
+  from pass_lift_inputs_and_assigns import lift_inputs_and_assigns
   from pass_lift_consts import lift_consts
-  from pass_lift_assigns import lift_assigns
-  from pass_pow import pow_replacement
+  from pass_dead_removal import dead_removal
+  from pass_simplify import simplify
   from output_rust import to_rust
   from pass_single_assignment import single_assignment
 
   data = get_runmain_input()
   exp = parse_function(data)
-  inputs = lift_inputs(exp)
-  assigns = lift_assigns(exp, inputs)
+  inputs, assigns = lift_inputs_and_assigns(exp)
   consts = lift_consts(exp, inputs, assigns)
-  pow_replacement(exp, inputs, assigns, consts)
+  dead_removal(exp, inputs, assigns, consts)
 
   exp = reverse_diff(exp, inputs, assigns, consts)
-  consts = lift_consts(exp, inputs, assigns, consts)
-  single_assignment(exp, inputs, assigns, consts)
+  simplify(exp, inputs, assigns, consts)
 
-  #func, new_inputs, new_consts = to_rust(exp, inputs, assigns, consts)
+  if len(sys.argv) == 3 and sys.argv[2] == "test":
+    assert(exp[0] == "Return")
+    tup = exp[1]
+    assert(tup[0] == "Tuple")
+    box = tup[2]
+    if box[0] == "Const":
+      const = box
+      assert(const[0] == "Const")
+      box = expand(const, assigns, consts)
 
-  print("reverse_diff:")
-  print_exp(exp)
-  print()
-  print_inputs(inputs)
-  print()
-  print_consts(consts)
-  print()
-  print_assigns(assigns)
+    assert(box[0] == "Box")
+    if len(box) == 1:
+      print("No input variables")
+      assert(len(inputs) == 0)
+      return
+
+    for name, diff in zip(inputs.keys(), box[1:]):
+        print("d{} = {}".format(name, expand(diff, assigns, consts)))
+
+  else:
+    single_assignment(exp, inputs, assigns, consts)
+    print("reverse_diff:")
+    print_exp(exp)
+    print()
+    print_inputs(inputs)
+    print()
+    print_consts(consts)
+    print()
+    print_assigns(assigns)
+
 
 if __name__ == "__main__":
   try:
