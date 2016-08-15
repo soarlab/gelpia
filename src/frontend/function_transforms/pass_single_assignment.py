aea86f9b49df8849c161d84e5e9653350967bc33
ian Briggs <ianbriggs@comcast.net>
2016-08-15 10:22:20 -0600

    Single assignment now only emmits as many expressions as needed

--- src/frontend/function_transforms/pass_single_assignment.py
+++ src/frontend/function_transforms/pass_single_assignment.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pass_manager import *
+from pass_utils import *
 
 import collections
 import re
@@ -9,10 +9,14 @@ import subprocess
 import os.path as path
 
 
-def single_assignment(exp, inputs, assigns, consts, name_cache=list()):
+def single_assignment(exp, inputs, assigns, consts):
 
   def cache(exp):
-    if exp[0] in {"Integer", "Float", "Variable", "InputInterval", "Input", "Const"}:
+    if exp[0] in {"Integer", "Float", "InputInterval",
+                  "PointInterval", "Input", "Const", "Tuple"}:
+      return exp
+    if exp[0] == "Variable":
+      assert(exp[1] in assigns)
       return exp
     key = cache_hash(exp)
     if key not in assigns:
@@ -29,9 +33,12 @@ def single_assignment(exp, inputs, assigns, consts, name_cache=list()):
       search_replace(var, val, e)
 
   def collapse(exp):
-    assert(exp[0] == "Tuple")
-    boxvars = str(assigns[exp[2][1]])
-    usages = "\n".join([str(k) for k in assigns.values()])
+    assert(exp[0] == "Return")
+    retval = exp[1]
+    boxvars = ""
+    if retval[0] == "Tuple":
+      boxvars = str(assigns[retval[2][1]])
+    usages = "\n".join([str(k) for k in assigns.values()]) + str(retval)
     single_used = dict()
     for var in assigns.keys():
       if usages.count(var) == 1 and boxvars.count(var) == 0:
@@ -40,29 +47,37 @@ def single_assignment(exp, inputs, assigns, consts, name_cache=list()):
       del assigns[var]
       for val in assigns.values():
         search_replace(var, single_used[var], val)
+      search_replace(var, single_used[var], exp)
 
   def _single_assignment(exp):
     typ = exp[0]
+
     if typ in {"Input", "InputInterval", "Const", "Integer", "Float", "Const"}:
       return exp
+
     if typ in BINOPS:
       left = cache(_single_assignment(exp[1]))
       right = cache(_single_assignment(exp[2]))
       return [typ, left, right]
+
     if typ in UNOPS:
       arg = cache(_single_assignment(exp[1]))
       return [typ, arg]
-    if typ in {"Variable"}:
+
+    if typ == "Variable":
       _single_assignment(assigns[exp[1]])
       return exp
-    if typ in {"Return"}:
+
+    if typ == "Return":
       ret = cache(_single_assignment(exp[1]))
-      return ret
-    if typ in {"Tuple"}:
+      return ["Return", ret]
+
+    if typ =="Tuple":
       exp[1] = cache(_single_assignment(exp[1]))
       exp[2] = cache(_single_assignment(exp[2]))
       return exp
-    if typ in {"Box"}:
+
+    if typ == "Box":
       for i in range(1, len(exp)):
         exp[i] = cache(_single_assignment(exp[i]))
       return exp
@@ -83,22 +98,20 @@ def single_assignment(exp, inputs, assigns, consts, name_cache=list()):
 
 def runmain():
   from lexed_to_parsed import parse_function
-  from pass_lift_inputs import lift_inputs
+  from pass_lift_inputs_and_assigns import lift_inputs_and_assigns
   from pass_lift_consts import lift_consts
-  from pass_lift_assigns import lift_assigns
-  from pass_pow import pow_replacement
+  from pass_simplify import simplify
 
   data = get_runmain_input()
   exp = parse_function(data)
-  inputs = lift_inputs(exp)
-  assigns = lift_assigns(exp, inputs)
+  inputs, assigns = lift_inputs_and_assigns(exp)
   consts = lift_consts(exp, inputs, assigns)
-  pow_replacement(exp, inputs, assigns, consts)
-
-  exp = single_assignment(exp, inputs, assigns, consts)
+  simplify(exp, inputs, assigns, consts)
   consts = lift_consts(exp, inputs, assigns, consts)
 
-  print("single_assignment:")
+  exp = single_assignment(exp, inputs, assigns, consts)
+
+  print("function:")
   print()
   print_exp(exp)
   print()
