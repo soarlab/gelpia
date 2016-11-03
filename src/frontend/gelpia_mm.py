0098d82ce5d3c2bf071f9bab2c4fa5617109f91f
Ian Briggs <ian.briggs@utah.edu>
2016-11-03 12:24:09 -0600

    Removed dependency on environ  closes #23

--- src/frontend/gelpia_mm.py
+++ src/frontend/gelpia_mm.py
@@ -2,14 +2,18 @@
 """ Runs gelpia in parallel in minimization mode and maximization mode for """
 """ FPTaylor's min_max_expr solver.                                        """
 
-""" Nota bene: Requires the environmental variable GELPIA to be set to the """
-""" path of the main gelpia bin.                                           """
-
 import multiprocessing
 import multiprocessing.pool as pool
 import subprocess
 import sys
 import os
+import os.path as path
+import shlex
+
+full_dir = path.abspath(path.dirname(sys.argv[0])) # Directory for this file
+base_dir = path.split(full_dir)[0] # One directory up
+bin_dir = path.join(base_dir, "bin")
+gelpia_exe = bin_dir+"/gelpia"
 
 MAX = None
 MIN = None
@@ -21,7 +25,7 @@ def get_max(tup):
     MAX = out.split('\n')[0]
   except:
     pass
-    #print("MAX FAILED:", out, '\n', err, file=sys.stderr)
+    print("MAX FAILED:", out, '\n', err, file=sys.stderr)
 
 def get_min(tup):
   out, err = tup
@@ -30,10 +34,11 @@ def get_min(tup):
     MIN = out.split('\n')[1]
   except:
     pass
-    #print("MIN FAILED:", out, '\n', err, file=sys.stderr)
+    print("MIN FAILED:", out, '\n', err, file=sys.stderr)
 
 def run_command(cmd):
-  p = subprocess.Popen(" ".join(cmd), shell=True, stdout=subprocess.PIPE,
+  cmd = shlex.split(' '.join(cmd))
+  p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
   out, err  = p.communicate()
   out = out.decode('utf-8')
@@ -42,8 +47,8 @@ def run_command(cmd):
 
 def main():
   p = multiprocessing.pool.ThreadPool(processes=2)
-  cmd1 = [os.getenv("GELPIA")] + sys.argv[1:]
-  cmd2 = [os.getenv("GELPIA"), "--dreal"] + sys.argv[1:]
+  cmd1 = [gelpia_exe, "-T"] + sys.argv[1:]
+  cmd2 = [gelpia_exe, "--dreal", "-T"] + sys.argv[1:]
   r1 = p.apply_async(run_command,
                      args=(cmd1[:],),
                      callback=get_max)
@@ -51,7 +56,7 @@ def main():
   r2 = p.apply_async(run_command,
                      args=(cmd2[:],),
                      callback=get_min)
-  
+
   r1.wait()
   r2.wait()
   p.close()
