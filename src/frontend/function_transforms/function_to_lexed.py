2e8ff07bc2ba3166e854917822659a774ce5bdf5
Ian Briggs <ianbriggsutah@gmail.com>
2019-09-17 12:37:45 -0600

    Lexer can be ran on dop files

--- src/frontend/function_transforms/function_to_lexed.py
+++ src/frontend/function_transforms/function_to_lexed.py
@@ -3,56 +3,56 @@
 import sys
 
 try:
-  import ply.lex as lex
+    import ply.lex as lex
 except:
-  print("PLY must be installed for python3", file=sys.stderr)
-  sys.exit(-1)
+    print("PLY must be installed for python3", file=sys.stderr)
+    sys.exit(-1)
 
 
 
 
 tokens = [
-  # Infix Operators
-  "PLUS",
-  "MINUS",
-  "TIMES",
-  "DIVIDE",
-  "INFIX_POW",
+    # Infix Operators
+    "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
+    "INFIX_POW",
 
-  # Prefix operators
-  "BINOP",
-  "UNOP",
+    # Prefix operators
+    "BINOP",
+    "UNOP",
 
-  # Variables
-  "NAME",
+    # Variables
+    "NAME",
 
-  # Assignment
-  "EQUALS",
+    # Assignment
+    "EQUALS",
 
-  # Literals
-  "INTEGER",
-  "FLOAT",
-  "INTERVAL",
-  "SYMBOLIC_CONST",
+    # Literals
+    "INTEGER",
+    "FLOAT",
+    "INTERVAL",
+    "SYMBOLIC_CONST",
 
-  # Deliminators
-  "LPAREN",
-  "RPAREN",
-  "LBRACE",
-  "RBRACE",
-  "COMMA",
-  "SEMICOLON",
+    # Deliminators
+    "LPAREN",
+    "RPAREN",
+    "LBRACE",
+    "RBRACE",
+    "COMMA",
+    "SEMICOLON",
 ]
 
 
 
 
 # Infix Operators
-t_PLUS      = '\+'
-t_MINUS     = '-'
-t_TIMES     = '\*'
-t_DIVIDE    = '/'
-t_INFIX_POW = '\^'
+t_PLUS      = "\+"
+t_MINUS     = "-"
+t_TIMES     = "\*"
+t_DIVIDE    = "/"
+t_INFIX_POW = "\^"
 
 
 # Prefix operators
@@ -69,47 +69,47 @@ t_UNOP = "({})".format(")|(".join(UNOPS))
 
 # Variables
 def t_NAME(t):
-  '([a-zA-Z]|\_)([a-zA-Z]|\_|\d)*'
-  if t.value in BINOPS:
-    t.type = "BINOP"
-  elif t.value in UNOPS:
-    t.type = "UNOP"
-  elif t.value in {"interval"}:
-    t.type = "INTERVAL"
-  elif t.value in SYMBOLIC_CONSTS:
-    t.type = "SYMBOLIC_CONST"
-  else:
-    t.type = "NAME"
+    "([a-zA-Z]|\_)([a-zA-Z]|\_|\d)*"
+    if t.value in BINOPS:
+        t.type = "BINOP"
+    elif t.value in UNOPS:
+        t.type = "UNOP"
+    elif t.value in {"interval"}:
+        t.type = "INTERVAL"
+    elif t.value in SYMBOLIC_CONSTS:
+        t.type = "SYMBOLIC_CONST"
+    else:
+        t.type = "NAME"
 
-  return t
+    return t
 
 
 # Assignment
-t_EQUALS = '='
+t_EQUALS = "="
 
 
 # Literals
-t_FLOAT   = ('('                 # match all floats
-             '('                 #  match float with '.'
-             '('                 #   match a number base
-             '(\d+\.\d+)'        #    <num.num>
-             '|'                 #    or
-             '(\d+\.)'           #    <num.>
-             '|'                 #    or
-             '(\.\d+)'           #    <.num>
-             ')'                 #
-             '('                 #   then match an exponent
-             '(e|E)(\+|-)?\d+'   #    <exponent>
-             ')?'                #    optionally
-             ')'                 #
-             '|'                 #  or
-             '('                 #  match float without '.'
-             '\d+'               #   <num>
-             '((e|E)(\+|-)?\d+)' #   <exponent>
-             ')'                 #
-             ')')
-t_INTEGER  = '\d+'
-t_INTERVAL = 'interval'
+t_FLOAT   = ("("                 # match all floats
+             "("                 #  match float with '.'
+             "("                 #   match a number base
+             "(\d+\.\d+)"        #    <num.num>
+             "|"                 #    or
+             "(\d+\.)"           #    <num.>
+             "|"                 #    or
+             "(\.\d+)"           #    <.num>
+             ")"                 #
+             "("                 #   then match an exponent
+             "(e|E)(\+|-)?\d+"   #    <exponent>
+             ")?"                #    optionally
+             ")"                 #
+             "|"                 #  or
+             "("                 #  match float without '.'
+             "\d+"               #   <num>
+             "((e|E)(\+|-)?\d+)" #   <exponent>
+             ")"                 #
+             ")")
+t_INTEGER  = "\d+"
+t_INTERVAL = "interval"
 
 
 # These are the tightest possible enclosures for these transcendental constants.
@@ -118,45 +118,45 @@ t_INTERVAL = 'interval'
 # constant. All intervals when represented by a GAOL interval have a width of
 # one ULP.
 SYMBOLIC_CONSTS = {
-  "pi"      : (("Float",
-                "3.141592653589793115997963468544185161590576171875"),
-               ("Float",
-                "3.141592653589793560087173318606801331043243408203125")),
-  "exp1"    : (("Float",
-                "2.718281828459045090795598298427648842334747314453125"),
-               ("Float",
-                "2.71828182845904553488480814849026501178741455078125")),
-  "half_pi" : (("Float",
-                "1.5707963267948965579989817342720925807952880859375"),
-               ("Float",
-                "1.5707963267948967800435866593034006655216217041015625")),
-  "two_pi"  : (("Float",
-                "6.28318530717958623199592693708837032318115234375"),
-               ("Float",
-                "6.28318530717958712017434663721360266208648681640625")),
+    "pi"      : (("Float",
+                  "3.141592653589793115997963468544185161590576171875"),
+                 ("Float",
+                  "3.141592653589793560087173318606801331043243408203125")),
+    "exp1"    : (("Float",
+                  "2.718281828459045090795598298427648842334747314453125"),
+                 ("Float",
+                  "2.71828182845904553488480814849026501178741455078125")),
+    "half_pi" : (("Float",
+                  "1.5707963267948965579989817342720925807952880859375"),
+                 ("Float",
+                  "1.5707963267948967800435866593034006655216217041015625")),
+    "two_pi"  : (("Float",
+                  "6.28318530717958623199592693708837032318115234375"),
+                 ("Float",
+                  "6.28318530717958712017434663721360266208648681640625")),
 }
 
 
 # Deliminators
-t_LPAREN    = '\('
-t_RPAREN    = '\)'
-t_LBRACE    = '\['
-t_RBRACE    = '\]'
-t_COMMA     = ','
-t_SEMICOLON = ';'
+t_LPAREN    = "\("
+t_RPAREN    = "\)"
+t_LBRACE    = "\["
+t_RBRACE    = "\]"
+t_COMMA     = ","
+t_SEMICOLON = ";"
 
 
 # Non-emmitting
-t_ignore = (' \t\n\r')
+t_ignore = (" \t\n\r")
 def t_comment(t):
-  '\#[^\n]*'
-  pass
+    "\#[^\n]*"
+    pass
 
 
 # Error
 def t_error(t):
-  print("Illegal character '{}'".format(t), file=sys.stderr)
-  sys.exit(-1)
+    print("Illegal character '{}'".format(t), file=sys.stderr)
+    sys.exit(-1)
 
 
 
@@ -166,18 +166,22 @@ def t_error(t):
 
 
 try:
-  from gelpia import bin_dir
-  _function_lexer = lex.lex(debug=False, optimize=True, outputdir=bin_dir,
-                            lextab="main_lextab.py")
+    from gelpia import bin_dir
+    _function_lexer = lex.lex(debug=False, optimize=True, outputdir=bin_dir,
+                              lextab="main_lextab.py")
 except:
-  _function_lexer = lex.lex(outputdir="__pycache__")
+    _function_lexer = lex.lex(outputdir="__pycache__")
 
-
-def lex_function(text):
-  return _function_lexer.lex(text)
 
 if __name__ == "__main__":
-  try:
-    lex.runmain(_function_lexer)
-  except KeyboardInterrupt:
-    print("\nGoodbye")
+    try:
+        from pass_utils import *
+        data = get_runmain_input()
+        _function_lexer.input(data)
+        while True:
+            tok = _function_lexer.token()
+            if not tok:
+                break
+            print(tok)
+    except KeyboardInterrupt:
+        print("\nGoodbye")
