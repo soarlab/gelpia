3425e9970bc10d26bc00fca83309a74832e334a1
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2020-06-12 18:26:43 -0600

    Solver basic

---
+++ src/solver/src/lib.rs
@@ -0,0 +1,15 @@
+extern crate futures;
+extern crate tokio;
+extern crate tokio_process;
+
+struct Solver {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
